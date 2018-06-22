b5843fa1969305e83d41d8f721029afb8d5ff4b5
Jordangoulet13 <40371862+Jordangoulet13@users.noreply.github.com>
2018-06-21 19:09:32 -0700

    Add files via upload

--- inc/Employee.class.php
+++ inc/Employee.class.php
@@ -19,25 +19,83 @@ class Employee implements ISelfRank {
       $this->phone = $ph;
       $this->jobTitle = $j;
       $this->department = $d;
+      $this->createEmail();
     }
     //Function to set email 
-    function createEmail($fN, $lN) {
-       return strtolower($fN).'.'.strtolower($lN).'@csis3280.net';
+    function createEmail() {
+       $this->email= strtolower($this->firstName).'.'.strtolower($this->lastName).'@csis3280.net';
     }
 
-    //The function below should be used to rank the employee based on
-    public function selfRank($term){
-
-      $searchRank = 0;
-
-      //Insert logic to rank employees, MUST use reflectionclass
-      $instReflector = new ReflectionClass('Employee');
-      foreach ($instReflector->getProperties() as $property) {
-      return $property->getName().":".$property->getValue($emp);
+    //The function Returns the position of where the term exists 
+    //relative to the beginning of the partial search string (independent of offset).
+    public function partialMatch($field, $partialSearches){
+      foreach($partialSearches as $term){
+        if(strpos($field,$term)!==false){
+          return true;
+        }
+      }
+    
+return false;
     }
 
 
-      //Matches ranking uses this priority
+    public function selfRank($searchTerm){
+      $partialSeachTerms=explode(' ',$searchTerm);
+      //$rankArray=[0,1,5,6,2,3];
+      $rank=0;
+      $instReflector= new ReflectionClass('Employee');
+
+      $emailProp=$instReflector->getProperties()[0];
+      
+      If (strtolower(strcmp($emailProp->getValue($this),$searchTerm)) === 0){
+        $rank=18;
+      }elseif (strtolower(strcmp($instReflector->getProperties()[1]->getValue($this),$searchTerm)) === 0){
+        $rank=17;
+      }elseif (strtolower(strcmp($instReflector->getProperties()[5]->getValue($this),$searchTerm)) === 0){
+        $rank=16;
+      }elseif (strtolower(strcmp($instReflector->getProperties()[6]->getValue($this),$searchTerm)) === 0){
+        $rank=15;
+      }elseif (strtolower(strcmp($instReflector->getProperties()[2]->getValue($this),$searchTerm)) === 0){
+        $rank=14;
+      }elseif (strtolower(strcmp($instReflector->getProperties()[3]->getValue($this),$searchTerm)) === 0){
+        $rank=13;
+      }elseif (strtolower(strpos($emailProp->getValue($this),$searchTerm)) !== false){
+        $rank=12;
+      }elseif (strtolower(strpos($instReflector->getProperties()[1]->getValue($this),$searchTerm)) !== false){
+        $rank=11;
+      }elseif (strtolower(strpos($instReflector->getProperties()[5]->getValue($this),$searchTerm)) !== false){
+        $rank=10;
+      }elseif (strtolower(strpos($instReflector->getProperties()[6]->getValue($this),$searchTerm)) !== false){
+        $rank=9;
+      }elseif (strtolower(strpos($instReflector->getProperties()[2]->getValue($this),$searchTerm)) !== false){
+        $rank=8;
+      }elseif (strtolower(strpos($instReflector->getProperties()[3]->getValue($this),$searchTerm)) !== false){
+        $rank=7;
+      }elseif (strtolower($this->partialMatch($emailProp->getValue($this),$partialSeachTerms)) !== false){
+        $rank=6;
+      }elseif (strtolower($this->partialMatch($instReflector->getProperties()[1]->getValues($this),$partialSeachTerms)) !== false){
+        $rank=5;
+      }elseif (strtolower($this->partialMatch($instReflector->getProperties()[5]->getValues($this),$partialSeachTerms)) !== false){
+        $rank=4;
+      }elseif (strtolower($this->partialMatch($instReflector->getProperties()[6]->getValues($this),$partialSeachTerms)) !== false){
+        $rank=3;
+      }elseif (strtolower($this->partialMatch($instReflector->getProperties()[2]->getValues($this),$partialSeachTerms)) !== false){
+        $rank=2;
+      }elseif (strtolower($this->partialMatch($instReflector->getProperties()[3]->getValues($this),$partialSeachTerms)) !== false){
+        $rank=1;
+      }else{
+        $rank=0;
+      }
+
+      // If Matches are identical ranking uses this priority
+      // i. Email = 18 
+      // ii. Phone Number = 17
+      // iii. First Name = 16
+      // iv. Last Name = 15
+      // v. Job Title = 14
+      // vi. Department = 13
+
+      // If Matches have the same charcters in the same positon
       // i. Email = 12
       // ii. Phone Number = 11
       // iii. First Name = 10
@@ -45,13 +103,15 @@ class Employee implements ISelfRank {
       // v. Job Title = 8
       // vi. Department = 7
 
-      //Partial matches
+      //Partial matches(cointains the same character, Regardless of position)
       // i. Email = 6
       // ii. Phone Number = 5
       // iii. First Name = 4
       // iv. Last Name = 3
       // v. Job Title = 2
       // vi. Department = 1
+
+      //No match = 0
 
       $this->rank = $searchRank;
     }
